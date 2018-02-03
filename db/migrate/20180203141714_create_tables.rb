521d63466a716333bb5730c20671cf85fcc7be3e
Morgan Fudge <mfudge92@gmail.com>
2018-02-03 14:36:50 +0000

    automatically backed up by learn

--- db/migrate/20180203141714_create_tables.rb
+++ db/migrate/20180203141714_create_tables.rb
@@ -2,7 +2,6 @@ class CreateTables < ActiveRecord::Migration[5.1]
   def change
     create_table :figures do |t|
       t.string :name
-      t.belongs_to :title, index: true
     end
 
     create_table :titles do |t|
